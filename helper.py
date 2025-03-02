41b38bc13d1a3b587be77cffede4530e3bbc34fb
David4870 <dandavid730@gmail.com>
2025-03-02 13:31:33 +0100

    a message without a hash #3

--- helper.py
+++ helper.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 import datetime
+import operator
 
 items = []
 
@@ -10,12 +11,16 @@ class Item:
     date: datetime.date
     isCompleted: bool = False
 
+
 def add(text, date):
+    if text == "":
+        text = "Todo"
     if len(date) == 0:
-        date = datetime.datetime.now().strftime('%Y-%m-%d')
-    text = text.replace('b', 'bbb').replace('B', 'Bbb')
-    date = datetime.datetime.strptime(date, '%Y-%m-%d')
+        date = datetime.datetime.now().strftime("%Y-%m-%d")
+    text = text.replace("b", "bbb").replace("B", "Bbb")
+    date = datetime.datetime.strptime(date, "%Y-%m-%d")
     items.append(Item(text, date))
+    items.sort(key=operator.attrgetter("date"))
 
 
 def get_all():
